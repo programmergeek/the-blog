5afc833261f809428158e8336f4741b2867b4816
Will <programergeek@gmail.com>
2022-06-26 15:15:19 +0200

    configure tailwindcss and download component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF7A00",
+
+          secondary: "#FFB800",
+
+          accent: "#FFC38C",
+
+          neutral: "#1c1917",
+
+          "base-100": "#FFF3E8",
+
+          info: "#3ABFF8",
+
+          success: "#22c55e",
+
+          warning: "#fde047",
+
+          error: "#ef4444",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
