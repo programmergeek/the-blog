fbb24d40158ffacdd6f1a264554c11dc918ea657
Will <programergeek@gmail.com>
2022-06-28 10:53:36 +0200

    create burger-menu icon

---
+++ src/Components/menu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Menu = () => {
+  return (
+    <svg
+      width="34"
+      height="22"
+      viewBox="0 0 34 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="34" height="6" rx="3" fill="#171717" />
+      <rect y="8" width="34" height="6" rx="3" fill="#171717" />
+      <rect y="16" width="34" height="6" rx="3" fill="#171717" />
+    </svg>
+  );
+};
