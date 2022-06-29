fd0c5078d9e0801f35ac5a9c7e917a02ea582ae8
Will <programergeek@gmail.com>
2022-06-29 21:23:30 +0200

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import { HeroBanner } from "./Components/HeroBanner/HeroBanner";
 import { NavBar } from "./Components/Nav-bar/NavBar";
 
 function App() {
   return (
-    <div className="App h-[200vh]">
-      <NavBar />
+    <div className="App main">
+      <div className="navigation border-black border-4">
+        <NavBar />
+      </div>
+      <div className="left-gutter border-black border-4 w-1/5"></div>
+      <div className="banner border-black border-4">
+        <HeroBanner />
+      </div>
+      <div className="masonry-grid border-black border-4"></div>
+      <div className="right-gutter border-black border-4 w-1/5"></div>
     </div>
   );
 }
