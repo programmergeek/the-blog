f88760477c71cb7b7d5a98aa378387ac37d8662d
Will <programergeek@gmail.com>
2022-06-30 14:59:34 +0200

    change text sizes for different screen sizes

--- src/Components/HeroBanner/HeroBanner.tsx
+++ src/Components/HeroBanner/HeroBanner.tsx
@@ -2,25 +2,25 @@ import React from "react";
 
 export const HeroBanner: React.FC = () => {
   return (
-    <div className="highlight w-full lg:h-auto bg-base-100 cursor-pointer sm:grid flex flex-col">
+    <div className="highlight w-full lg:h-auto bg-base-100 cursor-pointer lg:grid flex flex-col">
       <img
         src="https://picsum.photos/615/420"
         alt=""
         className=" w-full h-auto rounded-xl"
       />
       <div className="p-7">
-        <h5 className="text-left text-orange-400 sm:text-sm md:text-2xl">
+        <h5 className="text-left text-orange-400 sm:text-sm md:text-lg lg:text-xl 2xl:text-2xl">
           {" "}
           24 May 2022{" "}
         </h5>
         <h1
-          className={`sm:text-xl font-medium md:text-[56px] md:leading-[60px] md:mb-4 lg:text-7xl text-left hover:text-secondary`}
+          className={`font-medium text-2xl sm:text-3xl md:text-4xl md:mb-4 lg:text-5xl xl:text-6xl 2xl:text-7xl  text-left hover:text-secondary`}
           style={{ transition: "150ms" }}
         >
           Understanding closures in JavaScript
         </h1>
         <p
-          className={`text-left font-medium text-black sm:text-sm md:text-base`}
+          className={`text-left text-black text-xs sm:text-sm md:text-base lg:text-xl`}
           style={{ transition: "150ms" }}
         >
           Donec a velit ac lorem consectetur semper. Vestibulum felis neque,
