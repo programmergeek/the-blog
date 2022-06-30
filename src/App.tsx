ba2483182035d975c6bb0a4d86bb848f3ab978c3
Will <programergeek@gmail.com>
2022-06-30 16:10:28 +0200

    adjust gutter sizes and modify hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import { NavBar } from "./Components/Nav-bar/NavBar";
 function App() {
   return (
     <div className="App main">
-      <div className="navigation border-black border-4">
+      <div className="navigation">
         <NavBar />
       </div>
-      <div className="left-gutter border-black border-4 w-1/5"></div>
-      <div className="banner border-black border-4">
+      <div className="left-gutter w-1/5"></div>
+      <div className="banner border-4 border-black rounded-2xl">
         <HeroBanner />
       </div>
-      <div className="masonry-grid border-black border-4"></div>
-      <div className="right-gutter border-black border-4 w-1/5"></div>
+      <div className="masonry-grid"></div>
+      <div className="right-gutter w-1/5"></div>
     </div>
   );
 }
--- src/Components/HeroBanner/HeroBanner.tsx
+++ src/Components/HeroBanner/HeroBanner.tsx
@@ -2,25 +2,25 @@ import React from "react";
 
 export const HeroBanner: React.FC = () => {
   return (
-    <div className="highlight w-full lg:h-auto bg-base-100 cursor-pointer lg:grid flex flex-col">
+    <div className="highlight w-full lg:h-auto bg-base-100 cursor-pointer md:grid flex flex-col rounded-xl">
       <img
         src="https://picsum.photos/615/420"
         alt=""
-        className=" w-full h-auto rounded-xl"
+        className=" w-full h-auto rounded-tl-xl rounded-xl md:mt-3 md:ml-2 md:mb-3"
       />
-      <div className="p-7">
-        <h5 className="text-left text-orange-400 sm:text-sm md:text-lg lg:text-xl 2xl:text-2xl">
+      <div className="md:pl-5 pl-2 pb-2 md:pb-0 pt-2 pr-2 md:pr-5">
+        <h5 className="text-left text-orange-400 text-xs sm:text-sm md:text-md lg:text-lg 2xl:text-2xl">
           {" "}
           24 May 2022{" "}
         </h5>
         <h1
-          className={`font-medium text-2xl sm:text-3xl md:text-4xl md:mb-4 lg:text-5xl xl:text-6xl 2xl:text-7xl  text-left hover:text-secondary`}
+          className={`font-medium text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl  text-left hover:text-secondary`}
           style={{ transition: "150ms" }}
         >
           Understanding closures in JavaScript
         </h1>
         <p
-          className={`text-left text-black text-xs sm:text-sm md:text-base lg:text-xl`}
+          className={`text-left text-black text-xs md:text-sm xl:text-xl`}
           style={{ transition: "150ms" }}
         >
           Donec a velit ac lorem consectetur semper. Vestibulum felis neque,
@@ -28,8 +28,8 @@ export const HeroBanner: React.FC = () => {
           arcu eget lectus bibendum, vitae aliquet metus dignissim. Curabitur
           rutrum volutpat odio, eu varius sem.
         </p>
-        <div className="flex justify-start">
-          <button className="btn btn-wide mt-5">
+        <div className="flex md:hidden lg:flex justify-end lg:justify-start mr-2">
+          <button className="btn lg:btn-wide mt-5">
             <a href="/">Read Post</a>
           </button>
         </div>
