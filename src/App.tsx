7a7f2d4c1778202ad0ee409ae991d8aa52b49273
Will <programergeek@gmail.com>
2022-07-01 23:17:29 +0200

    create and implement props for hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,13 @@ function App() {
       <div className="App main">
         <div className="left-gutter w-1/5"></div>
         <div className="banner border-4 border-black rounded-2xl">
-          <HeroBanner />
+          <HeroBanner
+            image="https://picsum.photos/615/420"
+            date="24 May 2022"
+            href="/"
+            snippet="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet faucibus nisl vel faucibus. Quisque imperdiet, arcu vel luctus varius, metus sem pharetra nunc, a vulputate nunc magna et justo."
+            title="Understanding closures in JavaScript"
+          />
         </div>
         <div className="masonry-grid"></div>
         <div className="right-gutter w-1/5"></div>
--- src/Components/HeroBanner/HeroBanner.tsx
+++ src/Components/HeroBanner/HeroBanner.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 
-export const HeroBanner: React.FC = () => {
+interface Props {
+  image: string;
+  date: string;
+  title: string;
+  snippet: string;
+  href: string;
+}
+
+export const HeroBanner: React.FC<Props> = ({ ...props }) => {
   return (
     <div className="highlight w-full lg:h-auto bg-base-100 cursor-pointer sm:grid flex flex-col rounded-xl">
       <img
-        src="https://picsum.photos/615/420"
+        src={props.image}
         alt=""
         className=" w-full h-auto rounded-xl sm:mx-2 sm:my-2 "
       />
       <div className="md:pl-5 pl-4 pb-2 md:pb-0 pt-2 pr-2 md:pr-5 lg:p-7 xl:p-8">
         <h5 className="text-left text-orange-400 text-xs sm:text-sm md:text-md lg:text-lg 2xl:text-2xl">
-          {" "}
-          24 May 2022{" "}
+          {props.date}
         </h5>
         <h1
           className={`font-semibold sm:font-medium text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl  text-left hover:text-secondary`}
           style={{ transition: "150ms" }}
         >
-          Understanding closures in JavaScript
+          {props.title}
         </h1>
         <p
           className={`text-left text-black text-xs md:text-sm xl:text-xl`}
           style={{ transition: "150ms" }}
         >
-          Donec a velit ac lorem consectetur semper. Vestibulum felis neque,
-          vestibulum sed ligula sit amet, lobortis consectetur lacus. In semper
-          arcu eget lectus bibendum, vitae aliquet metus dignissim.
+          {props.snippet}
         </p>
         <div className="flex sm:hidden lg:flex justify-end lg:justify-start mr-2">
           <button className="btn lg:btn-wide mt-5">
-            <a href="/">Read Post</a>
+            <a href={props.href}>Read Post</a>
           </button>
         </div>
       </div>
