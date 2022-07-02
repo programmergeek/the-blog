52250781a0c6665215e632b869a3f681f05b9fd0
Will <programergeek@gmail.com>
2022-07-02 13:23:04 +0200

    increase border width and font weight of the tag component

--- src/Components/Tag/Tag.tsx
+++ src/Components/Tag/Tag.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Tag: React.FC = () => {
   return (
-    <span className="badge badge-lg badge-secondary mx-2 min-w-[75px] mt-0.5 badge-outline">
+    <span className="badge badge-secondary border-2 font-medium mx-2 min-w-[75px] mt-1 py-2 badge-outline">
       Test
     </span>
   );
