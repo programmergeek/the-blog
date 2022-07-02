a58303b85e4a3ce88350de3c25fdec9ff1ae3bfd
Will <programergeek@gmail.com>
2022-07-02 13:12:45 +0200

    create tag component

---
+++ src/Components/Tag/Tag.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Tag: React.FC = () => {
+  return (
+    <span className="badge badge-lg badge-secondary mx-2 min-w-[75px] mt-0.5 badge-outline">
+      Test
+    </span>
+  );
+};
