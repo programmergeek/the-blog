9761d0a6dfa07a8bd9e93b6ac66d9d887f05c1e4
Will <programergeek@gmail.com>
2022-07-14 15:40:27 +0200

    create masonry grid component

---
+++ src/Components/MasonryGrid/MasonryGrid.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+/**This component is meant to wrap around multiple card components.
+ * It controls the amount and size of the columns in the grid. */
+
+export const MasonryGrid: React.FC<Props> = ({ ...props }) => {
+  return <div></div>;
+};
