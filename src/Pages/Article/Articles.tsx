1a277b2fbd0af288bebd164136eb80782211b4f5
Will <programergeek@gmail.com>
2022-07-17 13:52:22 +0200

    create page layout for articles

--- src/Pages/Article/Articles.tsx
+++ src/Pages/Article/Articles.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { NavBar } from "../../Components/Nav-bar/NavBar";
 
 export const Articles: React.FC = () => {
-  return <div></div>;
+  return (
+    <NavBar>
+      <div className="article">
+        <div className="left-gutter border-2 border-black"></div>
+        <div className="article-content border-2 border-black"></div>
+        <div className="right-gutter border-2 border-black"></div>
+      </div>
+    </NavBar>
+  );
 };
