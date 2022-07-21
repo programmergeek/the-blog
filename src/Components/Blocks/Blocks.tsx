2aed1db0de2a44850c547f567e696b693b5aa482
Will <programergeek@gmail.com>
2022-07-21 12:10:30 +0200

    create block component

---
+++ src/Components/Blocks/Blocks.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { block } from "./types";
+
+/**Create content blocks depending on the type of data received
+ */
+export const Blocks: React.FC<block[]> = ({ ...props }) => {
+  return <div></div>;
+};
