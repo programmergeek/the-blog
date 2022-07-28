74bc298afa2945c3f8b956813410fd5f14057588
Will <programergeek@gmail.com>
2022-07-29 01:22:30 +0200

    give blockquote compnent a shadow

--- src/Components/Blocks/BlockQuote.tsx
+++ src/Components/Blocks/BlockQuote.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const BlockQuote: React.FC<Props> = ({ ...props }) => {
   return (
-    <blockquote className="flex bg-accent p-5 rounded-md">
+    <blockquote className="flex bg-accent p-5 rounded-md shadow-md">
       <div className="w-2 bg-primary rounded-sm"></div>
       <div className="ml-3">
         {props.value.children.map((text: any) => text.text)}
