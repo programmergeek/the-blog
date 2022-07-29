d9e3b0916b0657b1c06628372f259428a4dca0a5
Will <programergeek@gmail.com>
2022-07-29 04:49:31 +0200

    remove dummy data

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -5,13 +5,6 @@ import { NavBar } from "../../Components/Nav-bar/NavBar";
 import sanityClient from "../../client";
 import { urlFor } from "../../sanityFunctions";
 
-const cardData = {
-  href: "/",
-  snippet:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt maximus pretium. Morbi lacinia turpis vitae urna mollis mattis. Sed in eros luctus, eleifend sem id, tincidunt orci. Ut nec.",
-  title: "CSS Specificity",
-};
-
 export const Home = () => {
   const [posts, updatePosts] = useState<any>(null);
 
